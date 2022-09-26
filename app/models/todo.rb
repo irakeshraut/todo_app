6ec82ddb848f75e5ba754b416d26111df8943d2c
Rakesh Raut <irakeshraut@gmail.com>
2022-09-26 21:44:07 +1000

    finished broadcast

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,10 @@ class Todo < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit  -> { broadcast_prepend_to 'todos' }
+  # after_update_commit  -> { broadcast_replace_to "todos" }
+  # after_destroy_commit -> { broadcast_remove_to "todos" }
+
+  broadcasts_to ->(todo) { "todos" }, inserts_by: :prepend
 end
